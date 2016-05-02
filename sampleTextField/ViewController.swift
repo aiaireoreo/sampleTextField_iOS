0989b75f955bb01b57d7329b528cbe9c46d169f7
AI Matsubara <aiai.1116.info@gmail.com>
2016-05-02 22:00:06 +0800

    宿題終了

--- sampleTextField/ViewController.swift
+++ sampleTextField/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -17,19 +19,36 @@ class ViewController: UIViewController {
     
     //Returnキーが押されたとき、Did End On Exitに紐づくメソッドによって、キーボードが閉じる）
     
-    @IBAction func tapReturn(sender: UITextField) {
-        print(sender.text)
-        //入力された内容を取得するsender
-        
-        //TODO:ラベルを用意して、testという文字が含まれていたら「testです」とラベルに表示、それ以外は入力された時をそのまま表示しましょう。
-    }
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBOutlet weak var myTextField: UITextField!
+    
+    @IBAction func tapReturn(sender: UITextField) {
+        
+        
+        //入力された内容を取得するsender
+        print(myTextField.text)
+        
+        
+        //TODO:ラベルを用意して、testという文字が含まれていたら「testです」とラベルに表示、それ以外は入力された時をそのまま表示しましょう。
+        var str:String! = myTextField.text
+        if let r = str.rangeOfString("test"){
+            print("見つかった")
+            myLabel.text = "testです"
+        } else {
+            print("見つからない")
+            myLabel.text = myTextField.text
+        }
+    }
+    @IBAction func btnTap(sender: UIButton) {
+        myTextField.resignFirstResponder()
+    }
 
-
+    
+    
 }
-
